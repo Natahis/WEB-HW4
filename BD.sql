e1255269c71b329382bd755745f0b9f984cf1d24
Natahis <natahis@bk.ru>
2022-12-06 15:48:29 +0300

    HW4

---
+++ BD.sql
@@ -0,0 +1,13 @@
+
+-- create
+CREATE TABLE EMPLOYEE (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age TEXT NOT NULL,
+  address TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Наталья', '45', 'Москва');
+INSERT INTO EMPLOYEE VALUES (0002, 'Иван', '34', 'Воронеж');
+INSERT INTO EMPLOYEE VALUES (0003, 'Марья', '87', 'Казань');
